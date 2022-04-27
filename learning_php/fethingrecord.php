33768269cebef2db67fc0ad6ca4a88a89c7a212d
prabhakarsingh001 <prabhakar9374@gmail.com>
2022-04-27 16:05:35 +0530

    crud

---
+++ learning_php/fethingrecord.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>fetching records</title>
+</head>
+<body>
+
+<form  action=config.php method="post">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="emailid"  aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text" >We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  <?php
+  // include "config.php";
+  ?>
+
+<?php
+
+//TO UPDATE 
+
+ $sql= " UPDATE `form` SET `emailid` = 'SOM@gmail.com' WHERE `form`.`sno` = 37;";
+
+?>
+</body>
+</html>
